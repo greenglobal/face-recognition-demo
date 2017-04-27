3b95e38bdd3db1ac78ef3f2d3306c5dbf5cd7a47
Dong Nguyen <ndaidong@gmail.com>
2017-04-27 17:31:37 +0700

    Add flask-assets

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,37 @@
 # Import flask and template operators
-from flask import Flask, render_template
+from flask import Flask, url_for, render_template
+from flask_assets import Bundle, Environment
 
 # Define the WSGI application object
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # Configurations
 app.config.from_object('config')
 
+bundles = {
+  'common_css': Bundle(
+    'css/vendor/spectre.min.css',
+    'css/custom.css',
+    output='gen/common.css'
+  )
+}
+
+assets = Environment(app)
+
+assets.register(bundles)
+
 @app.route('/')
-def hello_world():
+def home():
   return render_template('home.html', title='Hello')
 
 @app.route('/object')
 @app.route('/object/<id>')
-def hello():
+def object():
   return render_template('object.html', id=id)
+
+@app.route('/css/<path:path>')
+def static_file(path):
+    return app.send_static_file('css/' + path)
 
 @app.errorhandler(404)
 def page_not_found(error):
